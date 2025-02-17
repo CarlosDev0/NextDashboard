19a3495da65ff0114a6398541a6d3a82c4fc47f2
Carlos Alberto Sanchez Alzate <ca.sanchez@globant.com>
2025-02-17 12:23:10 -0500

    query/route.ts get only in prod

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,23 +1,34 @@
- import { db } from "@vercel/postgres";
-
- const client = await db.connect();
-
- async function listInvoices() {
- 	const data = await client.sql`
-     SELECT invoices.amount, customers.name
-     FROM invoices
-     JOIN customers ON invoices.customer_id = customers.id
-     WHERE invoices.amount = 666;
-   `;
-
- 	return data.rows;
- }
+import { db, VercelPoolClient } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
 export async function GET() {
+  if (process.env.NODE_ENV === "production") {
+    return NextResponse.json(
+      { error: "Seeding is disabled in production" },
+      { status: 403 }
+    );
+  }
 
-   try {
-   	return Response.json(await listInvoices());
-   } catch (error) {
-   	return Response.json({ error }, { status: 500 });
-   }
+  if (!process.env.DATABASE_URL) {
+    return NextResponse.json(
+      { error: "Missing database connection." },
+      { status: 500 }
+    );
+  }
+  try {
+    const client = await db.connect();
+    return Response.json(await listInvoices(client));
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
+async function listInvoices(client: VercelPoolClient) {
+  const data = await client.sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+  return data.rows;
 }
