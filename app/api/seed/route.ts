58fbf901fca8e1e2c8588eafd5187ad46bfe5296
Carlos Alberto Sanchez Alzate <ca.sanchez@globant.com>
2025-02-17 12:03:06 -0500

    route.ts executes only in production

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -108,27 +108,29 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  if (!process.env.DATABASE_URL) {
-    return NextResponse.json(
-      { error: "Missing database connection." },
-      { status: 500 }
-    );
-  }
-  //return Response.json({
-  //  message:
-  //    'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  //});
-  try {
-    await client.sql`BEGIN`;
-    await seedUsers();
-    await seedCustomers();
-    await seedInvoices();
-    await seedRevenue();
-    await client.sql`COMMIT`;
+  if (process.env.NODE_ENV !== "production") {
+    if (!process.env.DATABASE_URL) {
+      return NextResponse.json(
+        { error: "Missing database connection." },
+        { status: 500 }
+      );
+    }
+    //return Response.json({
+    //  message:
+    //    'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    //});
+    try {
+      await client.sql`BEGIN`;
+      await seedUsers();
+      await seedCustomers();
+      await seedInvoices();
+      await seedRevenue();
+      await client.sql`COMMIT`;
 
-    return Response.json({ message: "Database seeded successfully" });
-  } catch (error) {
-    await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
+      return Response.json({ message: "Database seeded successfully" });
+    } catch (error) {
+      await client.sql`ROLLBACK`;
+      return Response.json({ error }, { status: 500 });
+    }
   }
 }
