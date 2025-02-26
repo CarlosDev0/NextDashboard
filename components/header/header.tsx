f635382459b93cd08c2b7c69dc7b8c1280e901fa
Carlos Alberto Sanchez Alzate <ca.sanchez@globant.com>
2025-02-26 16:50:03 -0500

    new image on header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,9 +1,12 @@
 import "./header.css";
 const header = () => {
   return (
-    <center>
-      <p className="mainHeader">App Multiservicios</p>
-    </center>
+    <div className="mainHeader">
+      <center>
+        <img src={"/logo.png"} className="mr-3 h-6 sm:h-9" alt="logo" />
+        App Multiservicios
+      </center>
+    </div>
   );
 };
 
