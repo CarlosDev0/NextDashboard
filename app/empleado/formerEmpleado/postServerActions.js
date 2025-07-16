4107ecef5210e9e9d6f9a8a5bd5fa1b1c1756b8e
CarlosDev0 <carlossan@gmail.com>
2025-07-16 10:37:22 -0500

    New Style for newEmployee form

--- app/empleado/formerEmpleado/postServerActions.js
+++ app/empleado/formerEmpleado/postServerActions.js
@@ -8,15 +8,15 @@ export const createEmployee = async (values) => {
   /*const [error, setError] = useState(null);*/
 
   let originalRequest = async (url, values) => {
-    let token =
-      "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJjYXJsb3MiLCJuYmYiOjE3MzU2NTQ2MjUsImV4cCI6MTczNTc0MTAyNSwiaWF0IjoxNzM1NjU0NjI1fQ.WDA5VxSEhjpTNDHViOvDi42c-4dkfvDWiZjslol8FZV5lqt40lgERgl6an-TRileLS_YGplPzb7vunDFVTEl6g";
+    // let token =
+    //   "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJjYXJsb3MiLCJuYmYiOjE3MzU2NTQ2MjUsImV4cCI6MTczNTc0MTAyNSwiaWF0IjoxNzM1NjU0NjI1fQ.WDA5VxSEhjpTNDHViOvDi42c-4dkfvDWiZjslol8FZV5lqt40lgERgl6an-TRileLS_YGplPzb7vunDFVTEl6g";
     let response = await fetch(url, {
       method: "POST",
       mode: "cors",
       cache: "no-cache",
       credentials: "same-origin",
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
         "Content-Type": "application/json",
       },
       redirect: "follow",
@@ -39,7 +39,7 @@ export const createEmployee = async (values) => {
   }
   const { nombre } = validatedFields.data;
   try {
-    let url = "http://localhost:5178/api/User/Register";
+    let url = process.env.NEXT_PUBLIC_API_URL + "/User/Register"; //"http://localhost:5178/api/User/Register";
     console.log("Before request_5");
     let { response, data } = await originalRequest(url, values);
     return { success: "Employee Created" };
